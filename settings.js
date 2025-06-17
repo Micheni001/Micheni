e11a47b59094b3067a9ebb3ed30c4eb3c67d2ee2
Micheni001 <alexmuthomi224@gmail.com>
2025-06-17 22:42:50 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Alex Micheni', // Your name
+  botOwner: 'Alex', // Your name
   ownerNumber: '254751026613', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
